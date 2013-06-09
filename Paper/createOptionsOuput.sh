3986a9b8a4bd427e3f33c9e7970b04259b256ee3
utke on macbook <utke on macbook@localhost>
2013-06-09 17:02:50 -0500

    in the makefile now

--- Paper/createOptionsOuput.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -u
-THISDIR=$PWD
-cd ${OPENADROOT}/xaifBooster/algorithms/TraceDiff/driver 
-./oadDriver | grep -v "caught exception" | grep -v "=== reset log" | grep -v "xaifBooster/algorithms/TraceDiff/driver" >  ${THISDIR}/tracingAlgOpts.txt
-cd ${THISDIR}
